67f33d37fcc77854d75dd4f517abc9d65c59ce92
Camoralesh27 <89227331+Camoralesh27@users.noreply.github.com>
2024-09-04 20:22:07 -0600

    validando datos, falta validar query y checkbox

--- main.js
+++ main.js
@@ -3,28 +3,25 @@ document.addEventListener('DOMContentLoaded', function() {
         firstName: '',
         lastName: '',
         email: '',
-        mensaje: '' 
+        message: '' 
     }
 
-    //Seleccionar los elementos de la interfaz
+    //Select the items from the DOM
     const form = document.querySelector('#form');
     const firstName = document.querySelector('#first-name');
     const lastName = document.querySelector('#last-name');
     const emailInput = document.querySelector('#email');
     const message = document.querySelector('#message');
-    const btnSubmit = document.querySelector('#btnSubmit');
-
+    const btnSubmit = document.querySelector('#submit');
     const query = document.querySelector('#query');
-    
 
-    //* Functions
-
-    // Change Radio Background
+    //* --> Change Radio Background
     const changeRadioBg = () => {
         const radio = query.querySelectorAll('input');
         
         radio.forEach((button) => {
             if (button.checked) {
+                console.log(button);
                 button.parentNode.classList.add('radio-selected');
             } else {
                 button.parentNode.classList.remove('radio-selected');
@@ -32,11 +29,86 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-
-    //* Eventos
-    // Add an event listener to the radio buttons
+    //* Events 
     query.querySelectorAll('input').forEach((button) => {
         button.addEventListener('click', changeRadioBg);
     });
+
+    firstName.addEventListener('input', validar);
+    lastName.addEventListener('input', validar);
+    emailInput.addEventListener('input', validar);
+    message.addEventListener('input', validar);
+
+    //* --> Validation
+    function validar(e) {
+        if(e.target.value.trim() === '') {
+            mostrarAlerta(`The field ${e.target.id} is required`, e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        } 
+
+        if((e.target.id === 'email' || e.target.id === 'cc') && !validarEmail(e.target.value)) {
+            mostrarAlerta('Please enter a valid email address', e.target.parentElement)
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        };
+
+
+        limpiarAlerta(e.target.parentElement);
+
+        //Asignar los valores
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+        
+        //Comprobar el objeto de email
+        comprobarEmail();
+
+        console.log(email);
+    }
+
+    function mostrarAlerta(mensaje, referencia) {
+
+        limpiarAlerta(referencia);
+
+        //Generar 'alerta' en HTML 
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('form__error');
+
+        //Inyectar 'error' a formulario
+        referencia.appendChild(error);
+    }
+
+    function limpiarAlerta(referencia){
+        //Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.form__error');
+        if(alerta) {
+            alerta.remove();
+        }
+    }
+
+    function validarEmail(email) {
+        //'expresion regular' para email en JS
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const resultado = regex.test(email)
+        return resultado;
+    }
+
+    const checkbox = document.querySelector('#consent');
+
+
+    function comprobarEmail() {
+        if(Object.values(email).includes('')) {
+            btnSubmit.classList.add('opacity-50')
+            btnSubmit.disabled = true;
+            return;
+        } 
+        
+        btnSubmit.classList.remove('opacity-50')
+        btnSubmit.disabled = false;
+    }
 });
+
+
 
